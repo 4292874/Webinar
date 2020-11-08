ddd62151ed7778e8766abb450761113fe1282597
root <sanketdeshmane@gmail.com>
2020-11-08 17:54:55 +0530

    resume

--- myresume/settings.py
+++ myresume/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e!n048o&x^z%zovm%w03)!8r+)n)+43d14*s@@d*11wnjk*^(z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = false
 
-ALLOWED_HOSTS = ['127.0.0.1', 'piyushresume.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'sanketdeshmane.herokuapp.com']
 
 
 # Application definition
